1c42096ed60707831291370cbd68bb852a285b1e
Rafay Ur Rehman <rafayrehman851@gmail.com>
2026-02-12 21:39:13 +0500

    Add files via upload
    Its just a beginning,I will move the real level of python.

---
+++ Print_output.py
@@ -0,0 +1,35 @@
+
+#Code for Day 01 :
+
+
+print("Welcome to Python Programming")
+
+# important point 
+
+# In python counts also spaces :
+
+print("instagram Id Rafay_shazada" + "facebook Id rafayrehman")
+
+# print the following code: one by one
+
+
+print(1 + 2) # its add two numbers 
+print(5 * 3) # its multiply two numbers 
+print(10 / 2)# its divide two numbers 
+print(7 - 3)# its subtract two numbers 
+
+print("Hello" + " World") 
+print("abc" * 3) # its print abc three times 
+
+# print Table of 9 using only print function  
+
+print(9*1)
+print(9*2)
+print(9*3)
+print(9*4)
+print(9*5)
+print(9*6)
+print(9*7)
+print(9*8)
+print(9*9)
+print(9*10)
