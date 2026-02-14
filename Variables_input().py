65b9b3a7d700e45b2c114141a8b6dee68fa64962
Rafay Ur Rehman <rafayrehman851@gmail.com>
2026-02-14 13:58:17 +0500

    Add files via upload
    Day 02

---
+++ Variables_input().py
@@ -0,0 +1,90 @@
+# Day02 answer
+
+x = 5
+y = x
+x = 10
+print(y)
+
+# output is 5
+'''When we write:
+
+x = 5
+y = x
+
+Python copies the value 5 into y.
+Later when we change:
+x = 10
+
+It does NOT change y, because y already stored 5.
+
+So the final output is: 5 '''
+'''
+🔥 Real-Life Analogy (Simple & Strong)
+
+Think of it like this:
+
+You write number 5 on two different papers.
+
+Then you erase and change one paper to 10.
+
+The other paper still has 5.
+
+Variables work the same way.'''
+
+
+
+
+
+
+
+# Now we add two numbers with help of variables 
+
+#This Program adds two numbers
+num1= 1
+num2= 2
+#add two numbers
+sum = num1+num2
+#display the sum
+
+print("the sum of the numbers is :", sum) # how print variable
+
+
+# Last day example solved with the help of variable 
+Rafay = "Rafay_shazada" + "instagram id " +  "snapchat id " + "rafaygshazada "
+print(Rafay) 
+
+
+# Print the following code one by one ?
+'''
+1. What is the value of x in x = 5? # store 5 in variable called x
+
+
+2. What is the value of result in result = 5 + 3? # print(result)
+'''
+
+# how to input from the user 
+
+# use input function  for take the input from the user  
+
+# input is built in function in python that allow us take the input from the user on output screen 
+
+# How to use :
+
+a = (input("enter a number"))
+b= (input("enter numeber "))
+
+#practice:
+
+# take the input from the user and then print (a) # remember one thing input default a string number 
+# take the input from the user and then print (b)
+
+# add two number using variables
+a = int(input("enter number 1"))
+b = int(input("enter number 2"))
+print("number a is: ",a)
+print("number b is: ",b)
+print("sum is" , a + b)
+
+
+#this topic discuss  tommarrow  further
+
